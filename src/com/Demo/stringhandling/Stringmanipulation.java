4adb8e155ba2bd2b0d4a5fc7730b3c66a56a7604
Sameer <sameerin84@gmail.com>
2021-09-09 14:39:44 +0530

    Add All Files

---
+++ src/com/Demo/stringhandling/Stringmanipulation.java
@@ -0,0 +1,17 @@
+package com.Demo.stringhandling;
+
+public class Stringmanipulation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+		String strFirstname= "Sameer";
+		String strLastname= "Bivalkar";
+		
+		System.out.println("Full name is : " + strFirstname + " " +  strLastname);
+		
+		
+
+	}
+
+}
